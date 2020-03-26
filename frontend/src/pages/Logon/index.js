9f97d8073f49ecc6784c69ced2f328ce905d9b00
jademir <jademirf@gmail.com>
2020-03-25 23:14:37 -0300

    fron-end finalizado dia 03

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,19 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import {FiLogIn} from 'react-icons/fi'
+import api from '../../services/api'
+
 import './style.css'
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon () {
+    const [id, setId] = useState('')
+    const history = useHistory()
+
+    async function handleLogoin (e) {
+        e.preventDefault()
+        
+        try {
+            const response = await api.post('/sessions', { id })
+
+            localStorage.ongId = id
+            localStorage.ongName = response.data.name
+
+            history.push('profile')
+        } catch (error) {
+            
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero"/>
-                <form action="">
+                <form onSubmit={handleLogoin}>
                     <h1>Faça seu logon</h1>
-                    <input type="text" placeholder="Sua ID" />
+                    <input 
+                        placeholder="Sua ID" 
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
 
                     <button className="button" type="submit">Entrar</button>
 
